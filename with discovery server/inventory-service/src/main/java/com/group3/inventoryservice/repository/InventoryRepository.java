b5238aaa4f20c65e5c73381186df27ba4b349c18
Shahara98 <shaharaameer@gmail.com>
2023-03-09 01:16:24 +0530

    Discovery server done

---
+++ with discovery server/inventory-service/src/main/java/com/group3/inventoryservice/repository/InventoryRepository.java
@@ -0,0 +1,10 @@
+package com.group3.inventoryservice.repository;
+
+import com.group3.inventoryservice.model.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+    List<Inventory> findBySkuCodeIn(List<String> skuCode);
+}
