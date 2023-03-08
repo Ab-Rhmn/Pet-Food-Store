7cfaf80124e83a7958562bfa93928f7bcf667702
abdul-rhmn <en91459@sjp.ac.lk>
2023-03-08 20:22:18 +0530

    all services are modules in microservice folder

---
+++ Pet-Food-Store-microservice/inventory-service/src/main/java/com/group3/inventoryservice/model/Inventory.java
@@ -0,0 +1,26 @@
+package com.group3.inventoryservice.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import jakarta.persistence.*;
+
+
+@Entity
+@Table(name = "t_inventory")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Inventory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String skuCode;
+    private Integer quantity;
+}
