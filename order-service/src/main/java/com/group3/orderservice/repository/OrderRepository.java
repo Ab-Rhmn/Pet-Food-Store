74904a5d6f5081630d566a8ad66459eeacf44ba5
abdul-rhmn <en91459@sjp.ac.lk>
2023-05-13 19:16:25 +0530

    cleaning

--- order-service/src/main/java/com/group3/orderservice/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.group3.orderservice.repository;
-
-import com.group3.orderservice.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
